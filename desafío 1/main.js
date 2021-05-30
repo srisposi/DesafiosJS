aa729bd2cfd51918f76063e9d1076d1f7b93c948
srisposi <srisposi@iiconcepcion.edu.ar>
2021-05-30 13:41:21 -0300

    Primer Desafío

---
+++ desafío 1/main.js
@@ -0,0 +1,5 @@
+let nombre = prompt ('Ingrese su Nombre');
+let apellido = prompt ('Ingrese su Apellido');
+
+alert("Se registro con Éxito");
+console.log("Se registro el usuario " + nombre + " " + apellido);
