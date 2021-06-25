e8d75f3ea4793a6898fa6962f66c63a599cf55a3
srisposi <srisposi@iiconcepcion.edu.ar>
2021-06-25 14:48:32 -0300

    desafio 5

--- desafío 5/js/main.js
+++ desafío 5/js/main.js
@@ -1,34 +1,31 @@
-//Se pide al usuario que elija el tipo de vehiculo
-tipoVehiculo=prompt("Elija su tipo de vehículo")
-//EL usuario selecciona el tipo de estadía que quiere
-estadia = prompt("Elija su tipo de estadía")
-//Traigo elementos con DOM
-const precios = document.getElementById(estadia);
-const vehiculo = document.getElementById(tipoVehiculo);
+const vehiculos = [
+    { id: 1, title: 'Moto', hora: 24, dia: 0, mes: 0, año: 0 },
+    { id: 2, title: 'Auto', hora: 0, dia: 0, mes: 0, año: 1 },
+    { id: 3, title: 'Pick Up', hora: 0, dia: 2, mes: 0, año: 0 },
+    { id: 4, title: 'Van', hora: 0, dia: 0, mes: 2, año: 0 }
+];
+  
 
+function renderHTML(array) {    
+    const vehiculosContainer = document.querySelector('article.vehiculosjs');
+    let html = '';
+    vehiculosContainer.innerHTML = '';  
+    
+    array.forEach(vehiculos => {
+    html += `
+        <tr>
+            <th>${vehiculos.title}</th>
+            <td>${vehiculos.hora}</td>
+            <td>${vehiculos.dia}</td>
+            <td>${vehiculos.mes}</td>
+            <td>${vehiculos.año}</td>
+        </tr>
+    `;
+    });
 
-//Se realiza el cobro del vehículo estacionado
-let costo = 0
-for(let i = 0; i < estadia ; i++){
-    costo = precios.textContent * vehiculo.textContent;
-    }
+    vehiculosContainer.innerHTML = html;
 }
-alert("Usted debe pagar " + costo)
-//Control de flujo con while - if para finalizar la compra
-let estadoCompra = prompt("¿Finalizó su compra?");
-while ((estadoCompra.toUpperCase() != "SI") || (estadoCompra.toUpperCase() != "NO")) {        
-    if((estadoCompra.toUpperCase() === "SI") || (estadoCompra.toUpperCase() === "NO")){
-        break;
-    }
-    else {
-        estadoCompra =prompt("Debe ingresar Si o No"); 
-    }
-}
-if(estadoCompra.toUpperCase() == "SI"){
-    alert("Usted debe pagar: " + costo)
-    alert("Gracias por usar nuestro sistema")
-}
-else{
-    alert("Usted no finalizo su compra, hasta ahora lleva comprado " + costo)
-    alert("Gracias por usar nuestro sistema")
-}
+
+renderHTML(vehiculos);
+
+console.log(vehiculosContainer);
